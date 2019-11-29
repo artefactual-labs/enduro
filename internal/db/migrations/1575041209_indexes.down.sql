8b872c70a9035ae87c91957516b4aae40fce7cc0
Jesús García Crespo <jesus@sevein.com>
2019-11-29 08:39:39 -0800

    db: add indexes to collection table

---
+++ internal/db/migrations/1575041209_indexes.down.sql
@@ -0,0 +1,7 @@
+DROP INDEX `collection_name_idx` ON `collection`;
+DROP INDEX `collection_transfer_id_idx` ON `collection`;
+DROP INDEX `collection_aip_id_idx` ON `collection`;
+DROP INDEX `collection_original_id_idx` ON `collection`;
+DROP INDEX `collection_pipeline_id_idx` ON `collection`;
+DROP INDEX `collection_status_idx` ON `collection`;
+DROP INDEX `collection_created_at_idx` ON `collection`;
