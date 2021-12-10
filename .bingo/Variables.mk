0757a170f5d8a26dabaa027f782b692c933aa675
Jesús García Crespo <jesus@sevein.com>
2021-12-10 08:27:40 +0100

    Install go-mod-upgrade

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -7,16 +7,22 @@ GO     ?= $(shell which go)
 
 # Below generated variables ensure that every time a tool under each variable is invoked, the correct version
 # will be used; reinstalling only if needed.
-# For example for goa variable:
+# For example for go-mod-upgrade variable:
 #
 # In your main Makefile (for non array binaries):
 #
 #include .bingo/Variables.mk # Assuming -dir was set to .bingo .
 #
-#command: $(GOA)
-#	@echo "Running goa"
-#	@$(GOA) <flags/args..>
+#command: $(GO_MOD_UPGRADE)
+#	@echo "Running go-mod-upgrade"
+#	@$(GO_MOD_UPGRADE) <flags/args..>
 #
+GO_MOD_UPGRADE := $(GOBIN)/go-mod-upgrade-v0.7.0
+$(GO_MOD_UPGRADE): $(BINGO_DIR)/go-mod-upgrade.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/go-mod-upgrade-v0.7.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=go-mod-upgrade.mod -o=$(GOBIN)/go-mod-upgrade-v0.7.0 "github.com/oligot/go-mod-upgrade"
+
 GOA := $(GOBIN)/goa-v3.4.3
 $(GOA): $(BINGO_DIR)/goa.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
