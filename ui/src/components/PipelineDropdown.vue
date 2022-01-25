7c930274a96dc43e455ca7538ea3a0acf54aafe3
Jesús García Crespo <jesus@sevein.com>
2022-01-25 21:20:58 +0100

    Add dropdowns to batch import form

---
+++ ui/src/components/PipelineDropdown.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="form-group">
+
+    <label for="pipeline-dropdown">Pipeline</label>
+
+    <select
+      class="form-control"
+      id="pipeline-dropdown"
+      @change="onChange"
+      required>
+
+      <option selected value>Select a pipeline</option>
+
+      <option
+        v-for="item in options"
+        v-bind:value="item.value">
+          {{ item.text }}
+      </option>
+
+    </select>
+
+    <small
+      id="pipeline-dropdown-help"
+      class="form-text text-muted">
+        Choose one of the pipelines configured.
+    </small>
+
+  </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator';
+import { api, EnduroPipelineClient } from '../client';
+
+@Component({})
+export default class PipelineDropdown extends Vue {
+
+  private options: Array<{ text: string, value: string }> = [];
+
+  private mounted(): void {
+    EnduroPipelineClient.pipelineList({}).then((response) => {
+      response.forEach((element) => {
+        if (element.id === undefined) {
+          return;
+        }
+        this.options.push({text: element.name, value: element.id});
+      });
+    });
+  }
+
+  // Emit the option or null.
+  private onChange(event: Event): void {
+    const select = event.target as HTMLSelectElement;
+    const option = this.options.filter((item) => item.value === select.value);
+    this.$emit('pipeline-selected', option.length ? option[0] : null);
+  }
+
+}
+
+</script>
---
+++ ui/src/components/PipelineProcessingConfigurationDropdown.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="form-group">
+
+    <label for="pipeline-processing-config-dropdown">Processing configuration</label>
+
+    <select
+      class="form-control"
+      id="pipeline-processing-configuration-dropdown"
+      described-by="pipeline-processing-configuration-dropdown-help"
+      v-on:change="$emit('pipeline-processing-configuration-selected', $event.target.value)"
+      required>
+
+      <option selected value>Select a processing configuration</option>
+
+      <option
+        v-for="item in options"
+        v-bind:value="item.value">
+          {{ item.text }}
+      </option>
+
+    </select>
+
+    <small
+      id="pipeline-processing-configuration-dropdown-help"
+      class="form-text text-muted">
+        Choose one of the processing configurations available.
+    </small>
+
+  </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import { api, EnduroPipelineClient } from '../client';
+
+@Component({
+  props: {
+    pipelineId: {
+      type: String,
+      required: false,
+    },
+  },
+})
+export default class PipelineProcessingConfigurationDropdown extends Vue {
+
+  private options: Array<{ text: string, value: string }> = [];
+
+  @Watch('pipelineId')
+  private onPipelineChanged(val: string, oldVal: string) {
+    if (!val) {
+      this.options = [];
+      return;
+    }
+    EnduroPipelineClient.pipelineProcessing({id: val}).then((response) => {
+      response.forEach((name) => {
+        this.options.push({text: name, value: name});
+      });
+    });
+  }
+
+}
+
+</script>
