5a95be3d013b4e75951bbf781396a701a3087d3c
Jesús García Crespo <jesus@sevein.com>
2022-02-14 16:41:37 +0100

    Update go-bagit

--- internal/bagit/bagit_test.go
+++ internal/bagit/bagit_test.go
@@ -11,7 +11,7 @@ import (
 func TestBagit(t *testing.T) {
 	assert.NilError(t, bagit.Complete("./tests/test-bagged-transfer"))
 	assert.ErrorContains(t, bagit.Complete("./tests/test-bagged-transfer-with-invalid-oxum"), "Payload-Oxum validation failed. Expected 1 files and 7 bytes but found 2 files and 7 bytes")
-	assert.ErrorContains(t, bagit.Complete("./tests/test-bagged-transfer-with-missing-manifest"), "Bag validation failed: tests/test-bagged-transfer-with-missing-manifest/manifest-sha256.txt does not exist;")
+	assert.ErrorContains(t, bagit.Complete("./tests/test-bagged-transfer-with-missing-manifest"), "Bag validation failed: tests/test-bagged-transfer-with-missing-manifest/manifest-sha256.txt does not exist")
 	assert.ErrorContains(t, bagit.Complete("./tests/test-bagged-transfer-with-unexpected-files"), "Bag validation failed: data/dos.txt exists on filesystem but is not in the manifest")
 	assert.ErrorContains(t, bagit.Complete("./tests/test-bagged-transfer-with-invalid-checksums"), "Bag validation failed: data/adios.txt sha256 validation failed")
 }
