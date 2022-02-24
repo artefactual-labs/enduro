ba3f19e884ba805581769e0421c5e5e30111500f
Jesús García Crespo <jesus@sevein.com>
2022-02-24 10:42:49 +0100

    Release pipeline earlier
    This commit updates the workflow to ensure that the pipeline is released as
    soon as possible, before the receipt delivery process starts.
    
    It's safe to call the releaser function multiple times.

--- internal/workflow/semaphore.go
+++ internal/workflow/semaphore.go
@@ -3,6 +3,7 @@ package workflow
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -14,8 +15,16 @@ import (
 	"github.com/artefactual-labs/enduro/internal/workflow/manager"
 )
 
-func acquirePipeline(ctx cadencesdk_workflow.Context, manager *manager.Manager, pipelineName string, colID uint) (bool, error) {
+type releaser func(ctx cadencesdk_workflow.Context, m *manager.Manager, pipelineName string) error
+
+// acquirePipeline acquires the pipeline semaphore. It returns a releaser that
+// users should execute. It's safe to execute more than once or when the acquire
+// operation failed (no-op).
+func acquirePipeline(ctx cadencesdk_workflow.Context, m *manager.Manager, pipelineName string, colID uint) (bool, releaser, error) {
 	var acquired bool
+
+	// The releaser defaults to a no-op operation, a nil value would panic.
+	var relfn releaser = func(ctx cadencesdk_workflow.Context, m *manager.Manager, pipelineName string) error { return nil }
 
 	// Acquire the pipeline semaphore.
 	{
@@ -26,34 +35,40 @@ func acquirePipeline(ctx cadencesdk_workflow.Context, manager *manager.Manager, 
 			WaitForCancellation:    false,
 		})
 		if err := cadencesdk_workflow.ExecuteActivity(ctx, activities.AcquirePipelineActivityName, pipelineName).Get(ctx, nil); err != nil {
-			return acquired, fmt.Errorf("error acquiring pipeline: %w", err)
+			return acquired, relfn, fmt.Errorf("error acquiring pipeline: %w", err)
 		}
+
+		acquired = true
 	}
 
-	acquired = true
+	// Create the function that releases the pipeline that we've just acquired.
+	var once sync.Once
+	relfn = func(ctx cadencesdk_workflow.Context, m *manager.Manager, pipelineName string) error {
+		if !acquired {
+			return nil
+		}
+		var err error
+		once.Do(func() {
+			ctx = withLocalActivityWithoutRetriesOpts(ctx)
+			ctx, _ = cadencesdk_workflow.NewDisconnectedContext(ctx)
+			err = cadencesdk_workflow.ExecuteLocalActivity(ctx, releasePipelineLocalActivity, m.Logger, m.Pipelines, pipelineName).Get(ctx, nil)
+			if err != nil {
+				err = fmt.Errorf("error releasing pipeline semaphore: %w", err)
+			}
+		})
+		return err
+	}
 
 	// Set in-progress status.
 	{
 		ctx := withLocalActivityOpts(ctx)
-		err := cadencesdk_workflow.ExecuteLocalActivity(ctx, setStatusInProgressLocalActivity, manager.Collection, colID, time.Now().UTC()).Get(ctx, nil)
+		err := cadencesdk_workflow.ExecuteLocalActivity(ctx, setStatusInProgressLocalActivity, m.Collection, colID, time.Now().UTC()).Get(ctx, nil)
 		if err != nil {
-			return acquired, fmt.Errorf("error updating collection status: %w", err)
+			return acquired, relfn, fmt.Errorf("error updating collection status: %w", err)
 		}
 	}
 
-	return acquired, nil
-}
-
-func releasePipeline(ctx cadencesdk_workflow.Context, manager *manager.Manager, pipelineName string) error {
-	ctx = withLocalActivityWithoutRetriesOpts(ctx)
-	ctx, _ = cadencesdk_workflow.NewDisconnectedContext(ctx)
-
-	err := cadencesdk_workflow.ExecuteLocalActivity(ctx, releasePipelineLocalActivity, manager.Logger, manager.Pipelines, pipelineName).Get(ctx, nil)
-	if err != nil {
-		return fmt.Errorf("error releasing pipeline semaphore: %w", err)
-	}
-
-	return nil
+	return acquired, relfn, nil
 }
 
 func releasePipelineLocalActivity(ctx context.Context, logger logr.Logger, registry *pipeline.Registry, pipelineName string) error {
